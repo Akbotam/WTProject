d94a6f59201a78436d07bc636be775c7051f01ec
Zharaskhan Aman <aman.zharaskhan@gmail.com>
2019-03-18 03:39:32 +0600

    Add home, detailm contact, clothes components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { DetailComponent } from './detail/detail.component';
+import { ClothesComponent } from './clothes/clothes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    DetailComponent,
+    ClothesComponent
   ],
   imports: [
     BrowserModule,
